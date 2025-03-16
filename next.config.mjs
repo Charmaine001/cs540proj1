8d61486d058535a2b3026309333b7d5fdb4bb092
Charmaine Aggson <agg601maine17@gmail.com>
2025-03-15 19:53:49 -0500

    Added basePath in attempt to fix gh-pages issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: "/cs540proj1", 
+    assetPrefix: "/cs540proj1/", 
     trailingSlash: true,
 };
 
